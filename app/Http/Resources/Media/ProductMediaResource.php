6cd6321f673f87f3eac0bd7e5e94268d40e0055f
tomvanhees <33604536+tomvanhees@users.noreply.github.com>
2020-10-04 21:28:35 +0200

    added ProductResource

--- app/Http/Resources/Media/ProductMediaResource.php
+++ app/Http/Resources/Media/ProductMediaResource.php
@@ -1,14 +1,28 @@
 <?php
 
-
 namespace App\Http\Resources\Media;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Http\Resources\AbstractResource;
-
-class ProductMediaResource extends MediaItemResource
+class ProductMediaResource extends JsonResource
 {
-    function getPath(): string
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "path" => $this->getPath(),
+            'order' => $this->order_column,
+        ];
+    }
+
+    private function getPath(): string
     {
         if ($this->data->hasGeneratedConversion("gallery")) {
             return $this->data->getFullUrl("gallery");
